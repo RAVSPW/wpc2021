91f8f8bebd1812c73f8548dfacef54cd6b860557
RAVSPW <34798524+RAVSPW@users.noreply.github.com>
2021-06-08 09:31:03 +0200

    Create aws_config.js

---
+++ src/aws_config.js
@@ -0,0 +1,9 @@
+export const awsConfig = {
+    UserPoolId: 'eu-central-1_vJ7tUmKmn',
+    IdentityPoolId: 'eu-central-1:18e707e3-9a9c-46fe-94e8-69bafb759871',
+    ClientId: 'ch42ilbt0306f7lc5qh2a91fv',
+    BucketName: 'test209739',
+    credentialsLoginsKey: 'cognito-idp.eu-central-1.amazonaws.com/eu-central-1_vJ7tUmKmn',
+    region:  'eu-central-1',
+    Region:  'eu-central-1',
+}
